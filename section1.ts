0a1c1c3a595fbb6973efd7438dbc6ac0f993a7f4
Damyan Peykov <dppeykov@gmail.com>
2019-10-30 14:42:26 +0100

    Adding an interface

--- section1.ts
+++ section1.ts
@@ -31,3 +31,26 @@ console.log(getInventoryItem("Testing"))
 // Using the any type will allow you to cast any type to any other - like the normal JS
 let originalCost: any = 425;
 originalCost = "a lot of money"
+
+// Using interfaces - better than the method in the getInventoryItem function above
+// The big advantage is that the interface can be used with any function and with as many as we want
+
+interface InventoryItem {
+    displayName: string;
+    inventoryType: string;
+    trackingNumber: string;
+    createDate: Date;
+    originalCost: number;
+}
+
+function getInventoryItemWithInterface(trackingNum: string): InventoryItem {
+    return {
+        displayName: "Package",
+        inventoryType: "pkg",
+        trackingNumber: trackingNum,
+        createDate: new Date(),
+        originalCost: 12222334
+    };
+}
+
+console.log(getInventoryItemWithInterface("Testing 2"))
